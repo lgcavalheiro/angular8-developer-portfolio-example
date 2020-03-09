33d3e194de353a39923daf1f6f17caa66567760e
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-03-09 22:46:16 +0000

    created base for components

---
+++ dev-portfolio/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    CurriculumComponent,
+    NavigationComponent,
+    ProjectsComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
