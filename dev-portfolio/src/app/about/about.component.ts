164fe3f2869d3a973495fa57d6fc5d6580c2ff46
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-03-11 01:52:51 +0000

    navigation and about almost done

--- dev-portfolio/src/app/about/about.component.ts
+++ dev-portfolio/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  info = {
+    greet: '',
+    experience: '',
+    work: '',
+  }
+
+  constructor() {
+    this.info.greet = `Hi! I'm Lucas Cavalheiro, glad you came by!`;
+    this.info.experience = `I'm a programmer with over 2 years of experience in the areas of telecommunications and banking.`;
+    this.info.work = `So far, i've worked maintaining I.T systems, developing software and single page applications.`;
+  }
 
   ngOnInit(): void {
+
   }
 
 }
--- dev-portfolio/src/app/app-routing.module.ts
+++ dev-portfolio/src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'curriculum',
+    component: CurriculumComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
